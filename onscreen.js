53a4fa50bfb630aa53db0d4787c760b8d387e430
hannesgregor <55529728+hannesgregor@users.noreply.github.com>
2023-01-30 19:17:22 +0200

    Update onscreen.js

--- onscreen.js
+++ onscreen.js
@@ -1,4 +1,5 @@
 const message = '<a data-sm-show-media-selection-on="click" href="javascript:void(0);">Contact Us</a>';
+message.color = "green";
 const phoneArr = Array.from(document.body.querySelectorAll(".phone-number"));
 
     sm.getApi({version: 'v1'}).then((glia) => {
