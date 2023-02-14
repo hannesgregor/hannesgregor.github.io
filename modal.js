3472e3e15e649a1cf446cc2af93ac4f36d0f6ce2
hannesgregor <55529728+hannesgregor@users.noreply.github.com>
2023-02-14 16:19:11 +0200

    Create modal.js

---
+++ modal.js
@@ -0,0 +1,16 @@
+sm.getApi({version: 'v1'}).then(function(salemove) {
+        salemove.getQueues().then(function(queues) {
+        var queueIds = queues.map(function(queue) {
+            return queue.id;
+    });
+    salemove.subscribeToQueueStateUpdates(queueIds, function(queue) {
+        var button = findButtonByQueue(queue.id);
+        if (queue.state.status === queue.state.STATUSES.OPEN) {
+            updateQueueButtonMedia(queue.state.medias);
+            showQueueButton(button);
+        } else {
+            hideQueueButton(button);
+        }
+})
+})
+});
