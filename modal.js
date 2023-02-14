88fae2259e148d39b0228188874c3c6d9cf25293
hannesgregor <55529728+hannesgregor@users.noreply.github.com>
2023-02-14 17:21:00 +0200

    Update modal.js

--- modal.js
+++ modal.js
@@ -8,7 +8,7 @@ sm.getApi({version: 'v1'}).then(function(salemove) {
     salemove.subscribeToQueueStateUpdates(queueIds, function(queue) {
         var queueButton = document.getElementById("btn");
         if (queue.state.status === queue.state.STATUSES.OPEN) {
-                console.log("Atleast 1 queue is open");
+                console.log("Atleast 1 queue is open and accepting visitors");
                     updateQueueButtonMedia(queue.state.medias);
                     showQueueButton(button);
         } else {
